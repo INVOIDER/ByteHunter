dabb1627be549c220ceb3444788eafd691fece0f
INVOIDER <5261323@gmail.com>
2024-03-24 15:48:26 +0300

    Installed and configured MongoDB
    1. Added MongoDB package for MongoDB
    2. Configured connection to MongoDB in index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,24 @@
 const express = require('express')
+const {MongoClient} = require('mongodb')
+require('dotenv').config()
 
 const PORT = process.env.PORT || 5000;
 
 const app = express()
+const dbClient = new MongoClient(process.env.DB_URL);
 
-app.listen(PORT,() => console.log(`Server started on port ${PORT}`))
+
+const startDB = async () =>{
+    try{
+        await dbClient.connect()
+        const db = dbClient.db("maindb")
+        await db.command({ ping: 1 }).then(res => console.log("DB status: ",res));
+    } catch (e){
+        console.log(e)
+    }
+}
+
+app.listen(PORT,() => {
+    console.log(`Server started on port ${PORT}`)
+    startDB();
+});
